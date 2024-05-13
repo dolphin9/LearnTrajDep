d593360fb6f2a6eea4d6675baee0b6037f343f19
dolphin9 <dolphin9@foxmail.com>
2024-05-13 08:44:42 +0100

    comfirmed

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python main.py --input_n 10 --output_n 25 --dct_n 35 --data_dir [PATH TO YOUR H36M DATASET]
+python main_3dpw.py --data_dir_3dpw "./dataset/mixed/" --input_n 10 --output_n 30 --dct_n 35 --exp "./log"
